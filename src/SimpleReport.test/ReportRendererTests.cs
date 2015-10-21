02ee3baea26cacffb04e05f2b76fc48fb2c41cf6
Joe <joe@kohde.io>
2015-10-21 07:38:27 +0200

    initial commit

---
+++ src/SimpleReport.test/ReportRendererTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleReports;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+namespace SimpleReport.Test
+{
+    [TestClass]
+    public class ReportRendererTests
+    {
+        [TestMethod]
+        public void GetParamters_Returns_OneParameter()
+        {
+            ReportRenderer renderer = new ReportRenderer();
+            var parameters = renderer.GetParameters(new { Foo = "Bar" });
+
+            Assert.IsTrue(parameters.ContainsKey("Foo"));
+            Assert.AreEqual(parameters["Foo"], "Bar");
+        }
+
+        [TestMethod]
+        public void GetReport_Returns_RenderedReport()
+        {
+            var reportDefinition = "<?xml version='1.0' encoding='utf-8'?><Report xmlns='http://schemas.microsoft.com/sqlserver/reporting/2008/01/reportdefinition' xmlns:rd='http://schemas.microsoft.com/SQLServer/reporting/reportdesigner'>  <Body>    <Height>2in</Height>    <Style />  </Body>  <Width>6.5in</Width>  <Page>    <PageHeight>29.7cm</PageHeight>    <PageWidth>21cm</PageWidth>    <LeftMargin>2cm</LeftMargin>    <RightMargin>2cm</RightMargin>    <TopMargin>2cm</TopMargin>    <BottomMargin>2cm</BottomMargin>    <ColumnSpacing>0.13cm</ColumnSpacing>    <Style />  </Page>  <AutoRefresh>0</AutoRefresh>  <ReportParameters>    <ReportParameter Name='Foo'>      <DataType>String</DataType>      <Prompt>ReportParameter1</Prompt>    </ReportParameter>  </ReportParameters>  <rd:ReportUnitType>Cm</rd:ReportUnitType>  <rd:ReportID>265d78f0-74e3-4905-954a-9c0b7be1b264</rd:ReportID></Report>";
+            byte[] byteArray = Encoding.ASCII.GetBytes( reportDefinition );
+            var memoryStream = new MemoryStream(byteArray);
+            
+            var reportRenderer = new ReportRenderer("FooDisplay", memoryStream);
+            reportRenderer.Initialise();
+
+            var data = new List<string>();
+            byte[] report = reportRenderer.GetReportAsPdf(data, new { Foo = "Bar" });
+
+            Assert.IsTrue(report.Length > 0);
+        }
+    }
+}
+
+
---
+++ src/SimpleReports/ReportRenderer.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Reporting.WebForms;
+using System.Web.UI.WebControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace SimpleReports
+{
+    public class ReportRenderer
+    {
+        ReportViewer ReportViewer { get; set; }
+
+        // CONFIG 
+        string displayName;
+        Stream reportDefinition;
+
+        public ReportRenderer()
+        {
+
+        }
+
+
+        public ReportRenderer(string displayName, Stream reportDefinition)
+        {
+            this.displayName = displayName;
+            this.reportDefinition = reportDefinition;
+        }
+
+        public void Initialise()
+        {
+            this.ReportViewer = new ReportViewer();
+            ReportViewer.Width = new Unit(100, UnitType.Percentage);
+            ReportViewer.Height = new Unit(100, UnitType.Percentage);
+            ReportViewer.EnableViewState = false;
+            ReportViewer.LocalReport.DisplayName = this.displayName;
+            ReportViewer.LocalReport.LoadReportDefinition(reportDefinition);
+            ReportViewer.LocalReport.EnableHyperlinks = true;
+            ReportViewer.ShowBackButton = false;
+            ReportViewer.ShowCredentialPrompts = false;
+            ReportViewer.ShowDocumentMapButton = false;
+            ReportViewer.ShowExportControls = false;
+            ReportViewer.ShowFindControls = false;
+            ReportViewer.ShowPageNavigationControls = false;
+            ReportViewer.ShowParameterPrompts = false;
+            ReportViewer.ShowPrintButton = false;
+            ReportViewer.ShowPromptAreaButton = false;
+            ReportViewer.ShowRefreshButton = false;
+            ReportViewer.ShowReportBody = false;
+            ReportViewer.ShowToolBar = false;
+            ReportViewer.ShowWaitControlCancelLink = false;
+            ReportViewer.ShowZoomControl = false;
+            ReportViewer.KeepSessionAlive = false;
+        }
+
+        public byte[] GetReportAsPdf<T>(List<T> data, dynamic param)
+        {
+            var reportDataSource = new ReportDataSource("data", data);
+            this.ReportViewer.LocalReport.DataSources.Add(reportDataSource);
+
+            Dictionary<string, object> reportProperties = GetParameters(param);
+
+            var listOfReportParameters = new List<ReportParameter>();
+            foreach (var dictvalue in reportProperties)
+            {
+                listOfReportParameters.Add(new ReportParameter(dictvalue.Key, Convert.ToString(dictvalue.Value)));
+            }
+            this.ReportViewer.LocalReport.SetParameters(listOfReportParameters);
+
+            string mimeType = string.Empty;
+            string encoding = string.Empty;
+            string fileNameExtension = string.Empty;
+
+            string deviceInfo = @"<DeviceInfo>
+		      <OutputFormat>PDF</OutputFormat>
+		      <PageWidth>21cm</PageWidth>
+		      <PageHeight>29.7cm</PageHeight>,
+		      <MarginTop>0.4cm</MarginTop>
+		      <MarginLeft>1.2cm</MarginLeft>
+		      <MarginRight>0.0cm</MarginRight>
+		      <MarginBottom>0.4cm</MarginBottom>
+		    </DeviceInfo>";
+
+            Warning[] warnings = null;
+            string[] streams = null;
+            byte[] renderedBytes = null;
+
+            //Render the report
+            renderedBytes = this.ReportViewer.LocalReport.Render(
+                "PDF", deviceInfo, out mimeType, out encoding, out fileNameExtension, out streams, out warnings);
+
+            return renderedBytes;
+        }
+
+        public Dictionary<string, object> GetParameters(dynamic param)
+        {
+            var properties = param.GetType().GetProperties();
+
+            var dictionary = new Dictionary<string, object>();
+            foreach (PropertyInfo prop in properties)
+            {
+                dictionary.Add(prop.Name, prop.GetValue(param));
+            }
+
+            return dictionary;
+        }
+    }
+}
